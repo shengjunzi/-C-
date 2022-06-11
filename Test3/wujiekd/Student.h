0eb6da81d27ad226c39003ca0cf9a4f9eb37939f
shengjunzi <52399325+shengjunzi@users.noreply.github.com>
2022-06-11 12:01:46 +0800

    Add files via upload
    通讯录系统

---
+++ Test3/wujiekd/Student.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+class Student {
+public:	char ID[20];         //记录学号
+		char name[20];       //记录姓名
+		char xing[20];       //记录性别
+		char grade[20];      //记录年级
+		char classs[20];     //记录班别
+		char program[20];    //记录程序设计成绩
+		char math[20];       //记录数学成绩
+		char lishan[20];     //记录离散成绩
+};
---
+++ Test3/wujiekd/resource.h
@@ -0,0 +1,70 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 wujiekd.rc 使用
+//
+#define COLOR_DEFAULT                   0
+#define COLOR_RED                       1
+#define IDR_MAINFRAME                   5
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_WUJIEKD_DIALOG              102
+#define DIALOG                          102
+#define IDI_ICON1                       133
+#define IDD_DIALOG1                     134
+#define delete_                         134
+#define IDD_DIALOG2                     135
+#define save                            135
+#define IDD_DIALOG3                     136
+#define import                          136
+#define IDD_DIALOG4                     138
+#define IDD_DIALOG5                     139
+#define change                          139
+#define IDD_DIALOG7                     141
+#define add                             141
+#define IDD_DIALOG8                     142
+#define menu                            142
+#define IDB_BITMAP1                     154
+#define IDB_BITMAP2                     155
+#define IDB_BITMAP3                     156
+#define IDB_BITMAP4                     174
+#define IDC_CHECK1                      1000
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_EDIT3                       1003
+#define IDC_EDIT4                       1004
+#define IDC_LIST2                       1005
+#define IDC_EDIT5                       1005
+#define IDC_BUTTON1                     1006
+#define IDC_BUTTON2                     1007
+#define IDC_EDIT6                       1007
+#define IDC_BUTTON3                     1008
+#define IDC_EDIT7                       1008
+#define IDC_BUTTON4                     1009
+#define IDC_RADIO1                      1009
+#define IDC_BUTTON5                     1010
+#define IDC_RADIO2                      1010
+#define IDC_BUTTON6                     1011
+#define IDC_BUTTON7                     1012
+#define IDC_BUTTON8                     1013
+#define IDC_RADIO5                      1014
+#define IDC_EDIT8                       1015
+#define IDC_EDIT9                       1016
+#define IDC_EDIT10                      1017
+#define IDC_EDIT11                      1018
+#define IDC_OK                          1020
+#define IDC_RADIO3                      1021
+#define IDC_LIST1                       1025
+#define IDC_COMBO1                      1026
+#define find_s                          57664
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        177
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
